1e0a015e9202c19a357481efc4818331391660fc
Jeff Anderson <jeffa@unlocalhost.com>
2025-04-25 23:17:20 +0000

    added items and ingredients

--- lamecraft/crafting.py
+++ lamecraft/crafting.py
@@ -1,11 +1,51 @@
 from .inventory import Inventory
 
+# List of raw ingredients that can be placed in the world
+INGREDIENTS = [
+    'wood', 'cobblestone', 'iron_ore', 'coal', 'sand', 'leather', 'string',
+    'feather', 'flint', 'cactus', 'sugar_cane', 'wheat', 'potato', 'carrot',
+    'bone', 'ink_sac', 'glass', 'clay', 'wool', 'egg'
+]
+
+# List of craftable items
+ITEMS = [
+    'plank', 'stick', 'torch', 'stone_pickaxe', 'iron_ingot', 'iron_pickaxe',
+    'bread', 'arrow', 'bow', 'cake'
+]
+
 class CraftingSystem:
     def __init__(self, recipes=None):
+        # Initialize with default recipes if none provided
         if recipes is None:
+            # recipes: tuple of input item names -> (output item, quantity)
             self.recipes = {
+                # Basic wood processing
                 ('wood',): ('plank', 4),
-                ('plank',): ('stick', 4)
+                ('plank',): ('stick', 4),
+
+                # Torches from wood and coal
+                ('wood', 'coal'): ('torch', 4),
+
+                # Stone pickaxe: 3 cobblestone + 2 sticks
+                ('cobblestone', 'cobblestone', 'cobblestone', 'stick', 'stick'): ('stone_pickaxe', 1),
+
+                # Smelt iron ore into iron ingot
+                ('iron_ore', 'coal'): ('iron_ingot', 1),
+
+                # Iron pickaxe: 3 iron ingots + 2 sticks
+                ('iron_ingot', 'iron_ingot', 'iron_ingot', 'stick', 'stick'): ('iron_pickaxe', 1),
+
+                # Bread from wheat
+                ('wheat', 'wheat', 'wheat'): ('bread', 1),
+
+                # Arrows: 1 feather + 1 stick -> 4 arrows
+                ('feather', 'stick'): ('arrow', 4),
+
+                # Bow: 3 string + 2 sticks
+                ('string', 'string', 'string', 'stick', 'stick'): ('bow', 1),
+
+                # Cake: 3 milk buckets (simulated as 'egg'), 2 sugar, 3 wheat
+                ('egg', 'sugar_cane', 'sugar_cane', 'sugar_cane', 'wheat', 'wheat', 'wheat'): ('cake', 1),
             }
         else:
             self.recipes = recipes
